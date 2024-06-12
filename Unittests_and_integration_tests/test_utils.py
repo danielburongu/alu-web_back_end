e59613c16b3757df3a37a3dfc83c66d0c433f459
danielburongu <d.burongu@alustudent.com>
2024-06-13 00:54:09 +0200

    memoize

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
 
@@ -39,6 +39,30 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(response, test_payload)
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            obj = TestClass()
+            # Accessing a_property twice
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            # Check that the method was only called once
+            mock_method.assert_called_once()
+
+            # Check that both results are correct
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
 
 if __name__ == '__main__':
     unittest.main()
