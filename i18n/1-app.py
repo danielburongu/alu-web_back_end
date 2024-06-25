4c9346a7d4ce387f61c26602863fd098cc456bf3
danielburongu <d.burongu@alustudent.com>
2024-06-25 14:48:26 +0200

    added 0-app.py

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python3
 """
-0.app i18n
+Flask App with Flask-Babel configuration.
+
+This module sets up a Flask application with Flask-Babel configured to support
+internationalization (i18n) with English and French languages.
+
+It includes a single route '/' which renders an HTML template.
+
+Requirements:
+    - Python 3.7
+    - Flask
+    - Flask-Babel
 """
+
+from typing import List
 from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
 
 class Config:
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """
+    Config class for configuring Flask-Babel.
+
+    Attributes:
+        LANGUAGES (list): List of supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale/language.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
+    LANGUAGES: List[str] = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE: str = 'en'
+    BABEL_DEFAULT_TIMEZONE: str = 'UTC'
 
 app.config.from_object(Config)
 
 babel = Babel(app)
 
 @app.route('/')
-def index():
-    """hello world"""
+def index() -> str:
+    """
+    Route to render the index HTML template.
+
+    Returns:
+        str: Rendered HTML content.
+    """
     return render_template('1-index.html')
 
 if __name__ == '__main__':
