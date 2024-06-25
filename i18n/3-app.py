5c803bc1daf7dcda4d9048ad27e24760896a7f9d
danielburongu <d.burongu@alustudent.com>
2024-06-25 19:47:18 +0200

    added 3-app.py translations1

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -9,18 +9,22 @@ from flask_babel import Babel, gettext as _
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """class config"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
     """best match locale lang"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
