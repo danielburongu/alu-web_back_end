a3d252b95fc83b91b38a574830023a281ed0fb73
danielburongu <d.burongu@alustudent.com>
2024-06-25 19:52:07 +0200

    added 3-app.py translations1

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -31,6 +31,7 @@ def index():
     """hello world"""
     return render_template("3-index.html", home_title=_("home_title"), home_header=_("home_header"))
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
