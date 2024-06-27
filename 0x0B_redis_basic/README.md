def88bbc2b41107390ed9be6fd37a76723281dfd
danielburongu <d.burongu@alustudent.com>
2024-06-27 09:36:28 +0200

    excercise.py

--- 0x0B_redis_basic/README.md
+++ 0x0B_redis_basic/README.md
@@ -1,1 +1,38 @@
-## Redis basic
+# Redis Cache Project
+
+## Description
+This project implements a simple cache system using Redis in Python. It includes a `Cache` class with methods to store data in Redis and retrieve it using a randomly generated key. 
+
+## Requirements
+- All files will be interpreted/compiled on Ubuntu 18.04 LTS using python3 (version 3.7)
+- All files should end with a new line
+- A README.md file, at the root of the folder of the project, is mandatory
+- The first line of all files should be exactly `#!/usr/bin/env python3`
+- The code should use the pycodestyle style (version 2.5)
+- All modules should have documentation
+- All classes should have documentation
+- All functions and methods should have documentation
+- All functions and coroutines must be type-annotated
+
+## Installation
+1. Ensure you have Python 3.7 and Redis installed.
+2. Install the Redis-py library: `pip install redis`
+
+## Usage
+```python
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,74 +1,32 @@
 #!/usr/bin/env python3
-"""Writing strings to Redis"""
-from functools import wraps
+"""
+Module for a Cache class to interact with Redis.
+"""
+import redis
 import uuid
-from redis.client import Redis
-from typing import Union, Callable, Optional, Any
-
-
-def count_calls(method: Callable) -> Callable:
-    """Incrementing values"""
-    data = method.__qualname__
-
-    @wraps(method)
-    def wrapper(self, *args, **kwds):
-        """wrapper func"""
-        self._redis.incr(data)
-        return method(self, *args, **kwds)
-    return wrapper
-
-
-def call_history(method: Callable) -> Callable:
-    """Storing lists"""
-    data = method.__qualname__
-    inputs = data + ":inputs"
-    outputs = data + ":outputs"
-
-    @wraps(method)
-    def wrapper(self, *args, **kwds):
-        """wrapper func"""
-        self._redis.rpush(inputs, str(args))
-        res = method(self, *args, **kwds)
-        self._redis.rpush(outputs, str(res))
-        return res
-    return wrapper
-
-
-def replay(method: Callable):
-    """Retrieving lists"""
-    data = method.__qualname__
-    inputs = data + ":inputs"
-    outputs = data + ":outputs"
-    redis = method.__self__._redis
-    count = redis.get(data).decode("utf-8")
-    print(f"{data} was called {count} times:")
-    in_list = redis.lrange(inputs, 0, -1)
-    out_list = redis.lrange(outputs, 0, -1)
-    redis_zipped = list(zip(in_list, out_list))
-    for a, b in redis_zipped:
-        attr, res = a.decode("utf-8"), b.decode("utf-8")
-        print(f"{data}(*{attr}) -> {res}")
-
+from typing import Union
 
 class Cache:
-    """Redis Class"""
-
+    """
+    Cache class for storing and retrieving data from Redis.
+    """
     def __init__(self):
-        """constructor"""
-        self._redis = Redis()
+        """
+        Initialize the Cache instance with a Redis client and flush the database.
+        """
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """returns a string"""
-        data = str(uuid.uuid4())
-        self._redis.set(data, data)
-        return data
-
-    def get(self, data: str, fn: Optional[Callable] = None) ->\
-            Union[str, bytes, int, float]:
-        """get method"""
-        if data:
-            res = self._redis.get(data)
-            return fn(res) if fn else res
+        """
+        Store the input data in Redis using a randomly generated key and return the key.
+        
+        Args:
+            data (Union[str, bytes, int, float]): The data to be stored in Redis.
+        
+        Returns:
+            str: The key under which the data is stored.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
