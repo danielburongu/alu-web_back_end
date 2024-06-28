f4fefc8eb18c2c9965a36ad8f4c29143cb007ed7
danielburongu <d.burongu@alustudent.com>
2024-06-28 20:01:52 +0200

    4-store.sql

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,7 +1,7 @@
---- Sql to create triggers.
-CREATE TRIGGER update_item_quantity AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
+-- SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW UPDATE items
+SET
+quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
