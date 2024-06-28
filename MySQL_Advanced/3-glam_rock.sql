e1c3dd4f681880f7e7e8a7e142f89a0c89c1c0b6
danielburongu <d.burongu@alustudent.com>
2024-06-28 19:42:34 +0200

    3-glam_rock.sql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- List bands with Glam rock as their main style, ranked by longevity
 SELECT band_name, 
-       YEAR(FROM_UNIXTIME(MAX(unix_timestamp(year_end, 'yyyy')) - MIN(unix_timestamp(year_start, 'yyyy')))) AS lifespan
+       YEAR(FROM_UNIXTIME(MAX(unix_timestamp(year_end, '2024')) - MIN(unix_timestamp(year_start, '2024')))) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 GROUP BY band_name
