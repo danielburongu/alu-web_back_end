29d6823e29a5d02eaf21c83a6c54ff8839e8cd1f
danielburongu <d.burongu@alustudent.com>
2024-06-28 19:56:22 +0200

    4-store.sql

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+--- Sql to create triggers.
+CREATE TRIGGER update_item_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
