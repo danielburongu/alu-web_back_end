e225ea29278e114f2a5ee1e6d70a0616bdd036c5
danielburongu <d.burongu@alustudent.com>
2024-07-10 20:12:58 +0200

    handling erro

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,5 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate fetching data from an API (e.g., using setTimeout)
-    setTimeout(() => {
-      const data = { message: "Data fetched from API" };
-      resolve(data); // Resolve with fetched data
-    }, 1000); // Simulate delay of 1 second
-  });
+function getResponseFromAPI() {
+  return Promise.resolve();
 }
+
+export default getResponseFromAPI;
